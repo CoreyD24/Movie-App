31506a4ccf4bcb6dd23905af31fbb311f2997be5
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-12-08 13:56:16 -0700

    Bunch of auth stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+require('dotenv').config()
 const app = express();
 
 const port = 3000;
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", require("./api"));
+app.use("/auth", require("./auth"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
